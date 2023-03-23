1f96db45de4011ca62481761a5db1f9637483f35
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-03-23 08:26:30 +0530

    Add files via upload

---
+++ src/Components/Smile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Smile = (props) => {
+   
+    return(
+        <div>
+          <h1>Smile Component</h1>
+          <p>It is a functional Component</p>
+          <img src="https://img.freepik.com/free-photo/world-smile-day-emojis-composition_23-2149024496.jpg"></img>
+        </div>
+    );
+};
+export default Smile;
